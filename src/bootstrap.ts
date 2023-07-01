8782564f69e75ef1aca257af785e29f75d54c6be
Sviatoslav <sviatoslavnazin@gmail.com>
2023-07-02 03:17:14 +0800

    add mf backend activity check

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,13 +2,18 @@ import express, { NextFunction, Request, Response, Router } from 'express';
 import dotenv from 'dotenv';
 import pinoHttp from 'pino-http';
 import Joi, { ObjectSchema } from 'joi';
+import fetch from 'cross-fetch';
 
 dotenv.config();
+
+const PORT = process.env.PORT ?? 3000;
+const BACKEND_SERVICES_URL = process.env.BACKEND_SERVICES_URL ?? '';
 
 enum ErrorType {
   VALIDATION_ERROR = 'validation_error',
   MICROFRONTEND_NOT_FOUND = 'microfrontend_not_found',
   MICROFRONTEND_ALREADY_STARTED = 'microfrontend_already_started',
+  BACKEND_SERVICE_NOT_FOUND = 'backend_service_not_found',
   SERVER_ERROR = 'server_error',
 }
 
@@ -21,34 +26,62 @@ type MicrofrontendDto = {
   name: string;
   url: string;
   component: string;
+  backend: string;
+};
+
+type StartedMicrofrontendDto = Omit<MicrofrontendDto, 'backend'> & {
+  isBackendActive: boolean;
+};
+
+type BackendServiceDto = {
+  domain: string;
+  port: number;
+  'status-code': number;
+};
+
+type GetBackendServicesResponse = {
+  services: BackendServiceDto[];
 };
 
 const MicrofrontendDtoSchema = Joi.object<MicrofrontendDto>({
   name: Joi.string().required(),
   url: Joi.string().required(),
   component: Joi.string().required(),
+  backend: Joi.string().required(),
 });
 
 const app = express();
-const port = process.env.PORT ?? 3000;
 const logger = pinoHttp({ transport: { target: 'pino-pretty' } });
 
 const router = Router();
 const microfrontendsRouter = Router();
 
-const startedMicrofrontends: MicrofrontendDto[] = [];
+const startedMicrofrontends: StartedMicrofrontendDto[] = [];
 
 microfrontendsRouter.get('/all', (_req, res) => {
   res.status(200).json(startedMicrofrontends);
 });
 
-microfrontendsRouter.post('/start', validateRequest(MicrofrontendDtoSchema), (req, res) => {
+microfrontendsRouter.post('/start', validateRequest(MicrofrontendDtoSchema), async (req, res) => {
   const data = req.body as MicrofrontendDto;
   const existingMicrofrontend = startedMicrofrontends.find((mf) => mf.url === data.url);
   if (existingMicrofrontend) {
     throw { status: ErrorStatus.BAD_REQUEST, type: ErrorType.MICROFRONTEND_ALREADY_STARTED };
   }
-  startedMicrofrontends.push(data);
+
+  let isBackendActive = false;
+
+  if (data.backend !== 'test') {
+    const backendServices = await getBackendServices();
+    const existingBackendService = backendServices.find((service) => createBackendUrl(service.domain, service.port) === data.backend);
+    if (!existingBackendService) {
+      throw { status: ErrorStatus.BAD_REQUEST, type: ErrorType.BACKEND_SERVICE_NOT_FOUND };
+    }
+
+    isBackendActive = existingBackendService['status-code'] === 200;
+  }
+
+  startedMicrofrontends.push({ name: data.name, url: data.url, component: data.component, isBackendActive });
   res.status(200).json({ success: true });
 });
 
@@ -73,8 +106,8 @@ app.use(errorMiddleware);
 
 const bootstrap = () => {
   try {
-    app.listen(port, () => {
-      console.log(`Microfrontend control server listening on port ${port}`);
+    app.listen(PORT, () => {
+      console.log(`Microfrontend control server listening on port ${PORT}`);
     });
   } catch (error) {
     console.error(`Failed to start Microfrontend control server: ${error}`);
@@ -104,4 +137,18 @@ function validateRequest(schema: ObjectSchema) {
     }
     next();
   };
+}
+
+async function getBackendServices() {
+  try {
+    const response = await fetch(`${BACKEND_SERVICES_URL}/services`);
+    const data = (await response.json()) as GetBackendServicesResponse;
+    return data.services;
+  } catch (error) {
+    console.error(`Failed to get backend services: ${error}`);
+  }
+}
+
+function createBackendUrl(domain: string, port: number) {
+  return `${domain}:${port}`;
 }
