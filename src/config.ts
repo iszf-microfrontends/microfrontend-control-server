a1890a542ddca9f9a690c89246ceb4161b11c9dc
Sviatoslav <sviatoslavnazin@gmail.com>
2023-10-15 16:26:13 +0800

    change CHECK_HEALTH_INTERVAL

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ import path from 'path';
 export const PORT = process.env.PORT;
 export const GET_BACKEND_SERVICES_URL = process.env.GET_BACKEND_SERVICES_URL ?? '';
 export const LOGS_PATH = path.resolve(__dirname, '../logs/combined.log');
-export const CHECK_HEALTH_INTERVAL = 1000;
+export const CHECK_HEALTH_INTERVAL = 10000;
